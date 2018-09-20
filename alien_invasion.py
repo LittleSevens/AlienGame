a98f7272a0731e9cc3423b59f853a74357c9601d
LittleBadSeven <zpxu920306@163.com>
2018-09-21 00:59:16 +0800

    Add ship on the bottome of game window and rebuild the structure.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,22 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
 	#创建一个屏幕对象
 	pygame.init()
+	
+	ai_settings = Settings()
 	#设置窗口大小
-	screen = pygame.display.set_mode((990,660))
+	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	#设置窗口的标题
 	pygame.display.set_caption("Alien Invasion")
-	
-	#设置背景色
-	bg_color = (230,230,230)
-	
+	ship = Ship(screen)
 	
 	while True:
 		#监听键盘和鼠标事件
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()		
-		screen.fill(bg_color)
-		#让最近绘制的屏幕可见
-		pygame.display.flip()
+		gf.check_events()
+		gf.update_screen(ai_settings,screen,ship)
 run_game()
