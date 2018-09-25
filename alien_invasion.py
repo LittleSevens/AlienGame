eabe1368bb8d71235cc670bd7700301ebf17c051
LittleBadSeven <zpxu920306@163.com>
2018-09-25 23:11:59 +0800

    Add bullets for ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 import game_functions as gf
 
 def run_game():
@@ -13,10 +14,12 @@ def run_game():
 	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	#设置窗口的标题
 	pygame.display.set_caption("Alien Invasion")
-	ship = Ship(screen)
-	
+	ship = Ship(ai_settings,screen)
+	bullets = Group()
 	while True:
 		#监听键盘和鼠标事件
-		gf.check_events(ship)
-		gf.update_screen(ai_settings,screen,ship)
+		gf.check_events(ai_settings,screen,ship,bullets)
+		ship.update()
+		bullets.update()
+		gf.update_screen(ai_settings,screen,ship,bullets)
 run_game()
